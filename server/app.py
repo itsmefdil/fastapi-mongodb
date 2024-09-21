95c57eb47138662b0f3fa188bd043e3f6bf9ea97
Fadilah Riczky <fadilahriczky07@gmail.com>
2024-09-21 08:18:22 +0700

    update

--- server/app.py
+++ server/app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from server.routes.book import router as BookRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["Root"])
